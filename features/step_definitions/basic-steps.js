aa81e4ef317e03dfd39bf072219b05a9b837dd81
Carrosen <carla1.rosen@gmail.com>
2019-04-09 16:00:07 +0200

    updated cucumber to be able to create a contact, and also the basic-step.js

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -1,9 +1,25 @@
-const { Given, Then } = require("cucumber");
+const { Given, Then, When } = require('cucumber')
 
-Given("I visit the site", function() {
-    return "pending";
-});
+Given('I visit the site', async function() {
+  return 'pending'
+})
 
-Then("I should see {string}", function(string) {
-    return "pending";
-});
+Then('I should see {string}', async function(content) {
+  return 'pending'
+})
+
+When('I click {string}', async function(string) {
+  return 'pending'
+})
+
+Then('I fill in {string} with {string}', async function(string, string2) {
+  return 'pending'
+})
+
+Then('I should have {int} contact in my address book', async function(int) {
+  return 'pending'
+})
+
+Then('I should not see {string}', async function(string) {
+  return 'pending'
+})
